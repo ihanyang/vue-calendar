dc2ddb4ba30cb85d65020fc5ce3e5aac7fcf723d
ihanyang <h513915503@163.com>
2016-05-20 10:15:31 +0800

    delete files

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import "./css/normalize.css"
-import "./css/common.css"
-import "./css/app.css"
-import "./css/calendar.css"
-
-import Vue from "vue"
-import App from "./app.vue"
-
-new Vue(App).$mount("#app")
