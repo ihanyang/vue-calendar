dbb765653fb1bb90aceef2477db05e91171a2cd8
hanyang <513915503@qq.com>
2016-09-30 14:44:38 +0800

    整理目录

--- demo/app.js
+++
@@ -1,9 +0,0 @@
-import "./css/normalize.css"
-import "./css/common.css"
-import "./css/app.css"
-//import "../src/css/calendar.css"
-
-import Vue from "vue"
-import App from "./app.vue"
-
-new Vue(App).$mount("#app")
